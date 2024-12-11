55379045b5659b119ae04c62592f6fe15cc1a071
mackan754 <marcus.jansson226@gmail.com>
2024-12-11 16:39:20 +0100

    Small changes

--- jdbc-soundgood/src/main/java/se/kth/iv1351/soundgoodjdbc/model/AvailableInstrument.java
+++ jdbc-soundgood/src/main/java/se/kth/iv1351/soundgoodjdbc/model/AvailableInstrument.java
@@ -1,10 +1,10 @@
 package se.kth.iv1351.soundgoodjdbc.model;
 
 public class AvailableInstrument implements AvailableInstrumentDTO {
-    private String brand;
-    private String price;
-    private String quantity;
-    private String instrumentName;
+    private final String brand;
+    private final String price;
+    private final String quantity;
+    private final String instrumentName;
 
     public AvailableInstrument(String instrumentName, String brand, String quantity, String price){
         this.brand = brand;
@@ -14,19 +14,23 @@ public class AvailableInstrument implements AvailableInstrumentDTO {
 
     }
     
+    @Override
     public String getBrand() {
         return brand;
 
     }
 
+    @Override
     public String getPrice(){
         return price;
     }
 
+    @Override
     public String getQuantity(){
         return quantity;
     }
 
+    @Override
     public String getInstrumentName(){
         return instrumentName;
     }
--- jdbc-soundgood/src/main/java/se/kth/iv1351/soundgoodjdbc/startup/Main.java
+++ jdbc-soundgood/src/main/java/se/kth/iv1351/soundgoodjdbc/startup/Main.java
@@ -1,5 +1,5 @@
 package se.kth.iv1351.soundgoodjdbc.startup;
-import se.kth.iv1351.soundgoodjdbc.controller.*;
+import se.kth.iv1351.soundgoodjdbc.controller.Controller;
 import se.kth.iv1351.soundgoodjdbc.integration.SoundgoodDBException;
 import se.kth.iv1351.soundgoodjdbc.view.BlockingInterpreter;
 
